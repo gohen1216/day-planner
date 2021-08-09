ae917f50b139e8eeafe52c2dac54b6438980378e
Sanusi Cole <sanusi1612@icloud.com>
2021-08-09 18:09:42 -0400

    worked on my index.js even more

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var inputgroupels = document.querySelectorAll (".input-group")
 
+var savebuttonhours = document.querySelectorAll("button")
 const date = new Date();
 
 console.log(date.getHours());
@@ -7,7 +8,22 @@ console.log(date.getHours());
 var currenthours = date.getHours();
 
 for(var i=0; i < inputgroupels.length; i ++) {
-    console.log (inputgroupels [i]) 
-    var blockhour = inputgroupels [i] .id
-    console.log (blockhour)
+    
+    var blockhour = Number(inputgroupels [i] .id)
+    console.log (blockhour) 
+    if(blockhour<currenthours){
+        inputgroupels[i].childNodes[3].classList.add("past")
+    }
+    else if(blockhour===currenthours){
+        inputgroupels[i].childNodes[3].classList.add("present")
+    }
+    else{
+        inputgroupels[i].childNodes[3].classList.add("future")
+    }
 }
+for(var i=0; i <  savebuttonhours.length; i ++){
+    savebuttonhours[i].addEventListener("click",function(event){
+        console.log (event.target.previousSibling.previousSibling.value)
+    })
+}
+
